14076ee7423e6af383524d15b32d3fb6bff307e8
jumakrk <jumakrk@gmail.com>
2024-05-12 19:50:48 +0300

    added dbconnect

---
+++ Model/dbconnect.js
@@ -0,0 +1,38 @@
+const dbConfig = require("../config/dbConfig.js");
+const {Sequelize, DataTypes} = require("sequelize");
+
+const Sequelize = new Sequelize();
+
+const sequelize = new Sequelize(
+            dbConfig.DB,
+            dbConfig.USER,
+            dbConfig.PASSWORD,{
+                host: dbConfig.HOST,
+                dialect: dbConfig.dialect,
+                    operatorAliases: false,  //if errors in your code will overwrite
+            }); 
+
+            sequelize
+            .authenticate()
+              //promise
+            .then(()=> {
+                console.log("Database Connection Successful...");
+            })
+            .catch((err)=>{
+                    console.log("Error" + err);
+            });
+
+            const db = {};
+
+            db.Sequelize = Sequelize;
+            db.sequelize = sequelize;
+
+db.sequelize.sync ({force:false})
+.then(()=>{
+console.log('re-sync done');
+})
+db.student = require( "./studentModel.js")(sequelize, DataTypes);
+db.courses = require( "./courseModel.js")(sequelize, DataTypes);
+db.users = require( "./authModel.js")(sequelize, DataTypes);
+
+module.exports = db;
