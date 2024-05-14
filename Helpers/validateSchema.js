16639c371173aacb4c81642f06dbc0a623d408f4
jumakrk <jumakrk@gmail.com>
2024-05-14 21:49:32 +0300

    added validate schema

--- Helpers/validateSchema.js
+++ Helpers/validateSchema.js
@@ -1,8 +1,16 @@
-const Joi = require ('joi');
+const Joi = require('joi');
 
-const authSchema = Joi.object ({
-  email: Joi.string().email().lowercase().required(),
-  password:Joi.string().min(8).required(),
+const authSchema = Joi.object({
+    email: Joi.string().email().lowercase().required(),
+    password: Joi.string ().min(6).required(),
 })
 
-module.exports = {authSchema}
+const vendorValidate = Joi.object({
+    name: Joi.string().required(),
+    quantity: Joi.string().required() ,
+    price_Ksh: Joi.string().required(),
+})
+module.exports = {
+    authSchema,
+    vendorValidate
+}
