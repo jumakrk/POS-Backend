9da9059e88a974ff7105a98e4f247444dc562ba6
jumakrk <jumakrk@gmail.com>
2024-05-14 21:46:42 +0300

    added jthelpers for token generation during login and sign up

--- Helpers/jwtHelpers.js
+++ Helpers/jwtHelpers.js
@@ -1,79 +1,70 @@
-const JWT = require ('jsonwebtoken');
-const createError = require ('http-errors');
-// const user = require ('../Model/userModel');
+const createError = require('http-errors');
+const JWT = require('jsonwebtoken');
 
 module.exports = {
-  signAccessToken: (UserId) => {
-    return new Promise ((resolve, reject) =>{
-      const payload = {UserId}
-      const secret = process.env.ACCESS_TOKEN_SECRET;
-      const options = {
-        expiresIn:'10m',
-        issuer: 'JumaKariuki',
-        audience: UserId.toString()
-      }
-      JWT.sign(payload, secret, options, (error, token)=> {
-        if(error) {
-          console.log(error.message)
-          reject(createError.message)
+signAccessToken: (userId) => {
+    return new Promise((resolve, reject) => {
+    const payload = { userId };
+    const secret = process.env.ACCESS_TOKEN_SECRET;
+    const options = {
+        expiresIn: '10m',
+        issuer: 'jumakrk.com',
+        audience: userId.toString(),
+    };
+    JWT.sign(payload, secret, options, (error, token) => {
+        if (error) {
+        console.log(error.message);
+        reject(createError.InternalServerError());
         }
-        resolve(token)
-      })
-    })
-  },
+        resolve(token);
+    });
+    });
+},
 
-  verifyAccessToken: (req, res, next)=> {
-    if(!req.headers['authorization']) return next (createError.Unauthorized())
-    const authHeader = req.headers['authorization']
-    const bearerToken = authHeader.split(' ')
-    const token = bearerToken[1]
-    JWT.verify(token, process.env.Access_Token_SECRET, (err, payload)=>{
-      if(err){
-        if(err.name === 'JsonWebTokenError'){
-          return next(createError.Unauthorized())
-        }else{
-          return next(createError.Unauthorized(err.message))
+verifyAccessToken: (req, res, next) => {
+    if (!req.headers['authorization']) return next(createError.Unauthorized());
+    const authHeader = req.headers['authorization'];
+    const bearerToken = authHeader.split(' ');
+    const token = bearerToken[1];
+    JWT.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, payload) => {
+    if (err) {
+        if (err.name === 'JsonWebTokenError') {
+        return next(createError.Unauthorized(err.message));
+        } else {
+        return next(createError.Unauthorized());
         }
-      }
-      req.payload = payload
-      next()
-    })
-  },
+    }
+    req.payload = payload;
+    next();
+    });
+},
 
-  signRefreshToken: (UserId)=>{
-    return new Promise ((resolve, reject)=>{
-      const payload = {}
-      const secret = process.env.Refresh_Token_SECRET;
-      const options = {
-        expiresIn:'1y',
-        issuer: 'JumaKariuki',
-        audience: UserId.toString()
-      }
-      JWT.sign(payload, secret, options, (error, token)=>{
-        if(error) {
-          console.log (error.message)
-          reject(createError.InternalServerError())
+signRefreshToken: (userId) => {
+    return new Promise((resolve, reject) => {
+    const payload = {};
+    const secret = process.env.REFRESH_TOKEN_SECRET;
+    const options = {
+        expiresIn: '1y',
+        issuer: 'Clifford.com',
+        audience: userId.toString(),
+    };
+    JWT.sign(payload, secret, options, (error, token) => {
+        if (error) {
+        console.log(error.message);
+        reject(createError.InternalServerError());
         }
-        resolve (token);
-      })
-    })
-  },
+        resolve(token);
+    });
+    });
+},
 
-  verifyRefreshToken: (req, res, next)=> {
-    if(!req.headers['authorization']) return next (createError.Unauthorized())
-    const authHeader = req.headers['authorization']
-    const bearerToken = authHeader.split(' ')
-    const token = bearerToken[1]
-    JWT.verify(token, process.env.Refresh_Token_SECRET, (err, payload)=>{
-      if(err){
-        if(err.name === 'JsonWebTokenError'){
-          return next(createError.Unauthorized())
-        }else{
-          return next(createError.Unauthorized(err.message))
-        }
-      }
-      req.payload = payload
-      next()
-    })
-  },
-}
+verifyRefreshToken: (refreshToken) => {
+    return new Promise((resolve, reject) => {
+    JWT.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, payload) => {
+        if (err) return reject(createError.Unauthorized());
+        const userId = payload.aud;
+        resolve(userId.toString());
+    });
+    });
+},
+};
