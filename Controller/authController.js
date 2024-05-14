997f8262a14f36dd13ada7c4093778272ef81133
jumakrk <jumakrk@gmail.com>
2024-05-14 20:29:37 +0300

    added codes to add a user to the database in the authController

--- Controller/authController.js
+++ Controller/authController.js
@@ -1,3 +1,25 @@
 const db = require('../Model/dbConnect');
 
 const User = db.users;
+
+module.exports = {
+    addUser: async (req, res, next) => {
+        try {
+        const { email, password } = await authSchema.validateAsync(req.body);
+        const exists = await User.findOne({ where: { email } });
+        if (exists) {
+            throw createError.Conflict(`${email} has already been registered`);
+        }
+        const newUser = new User({ email, password });
+        const savedUser = await newUser.save();
+    
+          const accessToken = await signAccessToken(savedUser.id); // Assuming ID is accessible directly
+        res.status(200).send({ accessToken });
+        } catch (error) {
+        console.log(error);
+        if (error.isJoi === true) error.status = 422;
+        next(error);
+        }
+    },
+// http methods
+}
